411dffb88d4221da0d8071ffcadc4008b2c58dc9
seth tery <isethguy@gmail.com>
2018-02-18 22:51:50 -0500

    ssl bump

--- devploy.sh
+++ devploy.sh
@@ -1,7 +1,2 @@
-eval $(docker-machine env dev)
-
-docker stop jtmain_node_app
-
-docker rm jtmain_node_app
-docker build . -t jtmain:latest -f node.dockerfile
-docker run -d --net=bridge --name jtmain_node_app -p 80:8080 jtmain:latest 
+sudo docker-compose build
+sudo docker-compose up
