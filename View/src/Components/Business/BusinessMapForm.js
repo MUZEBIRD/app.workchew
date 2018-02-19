a31703bcc041a7399fafe9a53a223c89b06b3112
seth tery <isethguy@gmail.com>
2018-02-18 21:20:19 -0500

    working with search input

--- View/src/Components/Business/BusinessMapForm.js
+++ View/src/Components/Business/BusinessMapForm.js
@@ -6,34 +6,17 @@ import { Route, Link } from 'react-router-dom'
 import urlService from '../../Services/urlService.js'
 import restService from '../../Services/restService.js'
 
-
 import { withGoogleMap, withScriptjs, GoogleMap, Marker } from "react-google-maps"
+import { MySearchBox } from '../MySearchBox.js'
 
-const {SearchBox} = require("react-google-maps/lib/components/places/SearchBox");
 const MapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBJa_W1JGWcoBM_nDKwneNzBnKuRcyDr6M&v=3.exp&libraries=geometry,drawing,places"
-
-const searchInpuStyle = {
-  boxSizing: `border-box`,
-  border: `1px solid transparent`,
-  width: `340px`,
-  height: `32px`,
-  marginTop: `8px`,
-  padding: `0 12px`,
-  borderRadius: `3px`,
-  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-  fontSize: `14px`,
-  outline: `none`,
-  textOverflow: `ellipses`,
-}
 
 const MyMapComponent = withScriptjs(withGoogleMap(function(props) {
 
   return (
     <div>
       <GoogleMap onBoundsChanged={ props.onBoundsChanged } ref={ props.onMapMounted } defaultZoom={ 12 } defaultCenter={ props.geo }>
-        <SearchBox onPlacesChanged={ props.onPlacesChanged } ref={ props.onSearchBoxMounted } bounds={ props.bounds } controlPosition={ 1 } onPlacesChanged={ props.onPlacesChanged }>
-          <input type="text" placeholder="Customized your placeholder" style={ searchInpuStyle } />
-        </SearchBox>
+        <MySearchBox {...props} />
         { props.isMarkerShown && <Marker position={ props.geo } /> }
       </GoogleMap>
     </div>
@@ -41,6 +24,7 @@ const MyMapComponent = withScriptjs(withGoogleMap(function(props) {
   )
 
 }))
+
 const refs = {}
 
 class BusinessMapForm extends Component {
