6e7ca1b1af7bc4386a304878f32265e169fe5a7c
seth tery <isethguy@gmail.com>
2018-05-27 11:27:01 -0400

    user interest

--- View/src/Components/SignUp/UserInterest.js
+++ View/src/Components/SignUp/UserInterest.js
@@ -38,20 +38,65 @@ class UserInterest extends Component {
     return (
 
       <MuiThemeProvider>
-        <div className="wholeView w-100 d-flex flex-column">
-          <div className="showView w-100 scroll-y container">
+        <div className="wholeView w-100 d-flex flex-column align-items-center">
+          <div className="showView w-100 scroll-y container d-flex flex-column align-items-center justify-content-center">
             <br/>
-            <h1 className='text-left'>How do you identify yourself?</h1>
+            <h1 className='text-left'>How do you identify yourself ?</h1>
             <h1 className='text-left'>Choose the one that fits the best.</h1>
-            <div className='row'>
-              <button className="btn btn-info">
-                Remote Worker
+            <div className='w-100'>
+              <button className="btn btn-secondary m-2">
+                UI/UX Politics Capital
               </button>
-              <button className="btn btn-info">
-                Entrepreneur
+              <button className="btn btn-secondary m-2">
+                Venture Startups Art
               </button>
-              <button className="btn btn-info">
-                Traveling Professional
+              <button className="btn btn-secondary m-2">
+                Law Entrepreneurship Creativity Social
+              </button>
+              <button className="btn btn-secondary m-2">
+                Writing Real Estate Consulting Web
+              </button>
+              <button className="btn btn-secondary m-2">
+                Development
+              </button>
+              <button className="btn btn-secondary m-2">
+                Graphic
+              </button>
+              <button className="btn btn-secondary m-2">
+                Design
+              </button>
+              <button className="btn btn-secondary m-2">
+                Photography Sales & Marketing Branding Public Relations
+              </button>
+              <button className="btn btn-secondary m-2">
+                Accounting Big Data Architecture Production
+              </button>
+              <button className="btn btn-secondary m-2">
+                Audio/Video
+              </button>
+              <button className="btn btn-secondary m-2">
+                Music/DJ
+              </button>
+              <button className="btn btn-secondary m-2">
+                Investing
+              </button>
+              <button className="btn btn-secondary m-2">
+                Personal
+              </button>
+              <button className="btn btn-secondary m-2">
+                Development
+              </button>
+              <button className="btn btn-secondary m-2">
+                Mobil App
+              </button>
+              <button className="btn btn-secondary m-2">
+                Property
+              </button>
+              <button className="btn btn-secondary m-2">
+                Intellectual
+              </button>
+              <button className="btn btn-secondary m-2">
+                Foo
               </button>
             </div>
           </div>
