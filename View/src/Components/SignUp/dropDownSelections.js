ddd7f535a5e73b6e8eae33796d5231b5fac8292f
seth tery <isethguy@gmail.com>
2018-07-14 20:15:53 -0400

    adding drop down options

---
+++ View/src/Components/SignUp/dropDownSelections.js
@@ -0,0 +1,157 @@
+var idustries = [
+  {
+    name: "Accounting/Finance/Banking"
+  },
+  {
+    name: "Consulting"
+  },
+  {
+    name: "Arts/Creative"
+  },
+  {
+    name: "Government"
+  },
+  {
+    name: "Healthcare"
+  },
+  {
+    name: "Hospitality"
+  },
+  {
+    name: "Legal"
+  },
+  {
+    name: "Marketing/Public Relations/Social Media"
+  },
+  {
+    name: "Nonprofit"
+  },
+  {
+    name: "Technology"
+  },
+  {
+    name: "Education"
+  },
+  {
+    name: "Project Management"
+  },
+  {
+    name: "Law"
+  },
+  {
+    name: "Design"
+  },
+  {
+    name: "Illustration"
+  },
+  {
+    name: "Social Media"
+  },
+  {
+    name: "Other"
+  },
+  {
+    name: "Restaurant/Hospitality"
+  },
+  {
+    name: "Government Contracting"
+  },
+  {
+    name: "Not-for-Profit"
+  },
+  {
+    name: "Education"
+  },
+  {
+    name: "Sales"
+  },
+  {
+    name: "Real Estate"
+  }
+]
+
+var occupations = [
+
+  {
+    name: "Consultant"
+  },
+  {
+    name: "Freelancer"
+  },
+  {
+    name: "Entrepreneur"
+  },
+  {
+    name: "Executive/Mgmt"
+  },
+  {
+    name: "Salesperson"
+  },
+  {
+    name: "Designer"
+  },
+  {
+    name: "Student"
+  },
+  {
+    name: "Attorney"
+  },
+  {
+    name: "Programmer"
+  },
+  {
+    name: "Realtor"
+  },
+  {
+    name: "Journalist"
+  },
+  {
+    name: "Engineer"
+  },
+  {
+    name: "Writer"
+  },
+  {
+    name: "Remote Worker"
+  },
+  {
+    name: "Lobbyist "
+  },
+  {
+    name: "Photographer"
+  },
+  {
+    name: "Other"
+  }
+];
+
+var hopings = [
+
+  {
+    name: "Clients"
+  },
+  {
+    name: "Mentorship"
+  },
+  {
+    name: "Networking"
+  },
+  {
+    name: "Social Connections"
+  },
+  {
+    name: "Educational Opportunities"
+  },
+  {
+    name: "Workspace Variety"
+  },
+  {
+    name: "Job"
+  },
+  {
+    name: "Investor Capital"
+  }
+]
+
+
+export { industries, hopings, occupations }
