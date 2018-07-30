6eb50c208fd3dc2f31307e1bcdd32b491afb4c2e
seth tery <isethguy@gmail.com>
2018-07-29 23:56:17 -0400

    subsciption buttons

--- Services/wc-paypal.js
+++ Services/wc-paypal.js
@@ -6,9 +6,17 @@ mb-buyer@workchew.com
 runner08
 */
 
+var basePayPalUrl = "https://api.sandbox.paypal.com";
+
 const Rx = require('rxjs');
 
 var paypal = require('paypal-rest-sdk');
+
+
+var request = require('request')
+
+var client_id = 'ARlX_FQiPMWYBBxHdmAuPnIh4lF0YChw3Ju8R6oPiFfPS7NW74VcNadMQmDuXUmOwlKO215-DIZ4rtVp';
+var client_secret = 'EIkza4HNFtXuq9LBIulUqlzmyHxuUbtm7fR5zo5FnobdLCVQgu7l6Zp6hrI0xbn4eG9oqRuXv7O2dgnD';
 
 const payPalSandBoxConfig = {
   'mode': 'sandbox', //sandbox or live
@@ -40,6 +48,137 @@ var getPayment = function(paymentId) {
   })
 
 } //getPayment
+
+var getAccessToken = function() {
+
+  return Rx.Observable.create(function(observer) {
+
+    request.post({
+      url: basePayPalUrl + '/v1/oauth2/token?grant_type=client_credentials',
+
+      headers: {
+
+        "Content-Type": "x-www-form-urlencoded",
+        "Authorization": "Basic " + Buffer.from(client_id + ":" + client_secret).toString('base64')
+
+      }
+
+    }, function(err, httpResponse, body) {
+
+      observer.next(JSON.parse(body))
+      observer.complete()
+
+    })
+
+  })
+
+}
+
+var createStaterAgreement = function(data) {
+
+  return Rx.Observable.create(function(observer) {
+
+    var d = new Date();
+    var start_date = d.toISOString();
+
+
+    request.post({
+      url: basePayPalUrl + '/v1/payments/billing-agreements',
+
+      headers: {
+
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + Buffer.from(client_id + ":" + client_secret).toString('base64')
+
+      },
+      body: JSON.stringify({
+        "name": "Stater Agreement",
+        "description": "stater agreement for " + data.userEmail,
+        "start_date": start_date,
+        "payer": {
+          "payment_method": "paypal",
+          "payer_info": {
+            "email": data.userEmail
+          }
+        },
+        "plan": {
+          "id": data.planID
+        }
+      })
+
+    }, function(err, httpResponse, body) {
+
+      observer.next(JSON.parse(body))
+      observer.complete()
+
+    })
+
+  })
+
+}
+
+var excuteAgreement = function(data) {
+
+  return Rx.Observable.create(function(observer) {
+
+    request.post({
+      url: basePayPalUrl + '/v1/payments/billing-agreements/' + data.agreementToken + '/agreement-execute',
+
+      headers: {
+
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + Buffer.from(client_id + ":" + client_secret).toString('base64')
+
+      }
+
+    }, function(err, httpResponse, body) {
+
+      observer.next(JSON.parse(body))
+      observer.complete()
+
+    })
+
+  })
+
+}
+
+var getAgreementDetials = function(data) {
+
+  return Rx.Observable.create(function(observer) {
+
+    request.get({
+      url: basePayPalUrl + '/v1/payments/billing-agreements/' + data.agreementID,
+
+      headers: {
+
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + Buffer.from(client_id + ":" + client_secret).toString('base64')
+
+      }
+
+    }, function(err, httpResponse, body) {
+
+      observer.next(JSON.parse(body))
+      observer.complete()
+
+    })
+
+  })
+
+}
+
+
+getAgreementDetials({
+  agreementID: "I-YS2554FTVFYU"
+})
+
+  .subscribe((done) => {
+
+    var {id, agreement_details} = JSON.parse(done)
+
+    console.log(agreement_details)
+
+  })
 
 module.exports = {
   getPayment
