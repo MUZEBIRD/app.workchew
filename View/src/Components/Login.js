43fdfb5925ac9e82e3ee5e1d6b5c576ed1eb1454
seth tery <isethguy@gmail.com>
2018-08-02 23:47:21 -0400

    removing facebook sign up

--- View/src/Components/Login.js
+++ View/src/Components/Login.js
@@ -270,9 +270,9 @@ class Login extends Component {
         </div>
         <div className='d-flex' style={ { backgroundColor: 'white', position: 'relative', zIndex: '5' } }>
           <div className='flex-1'>
-            <br/>
-            <br/>
-            <FacebookLogin autoLoad={ false } appId="1755466141213974" fields="email,picture,first_name,last_name" callback={ this.responseFacebook } />
+            { /* <br/>
+                                      <br/>
+                                    <FacebookLogin autoLoad={ false } appId="1755466141213974" fields="email,picture,first_name,last_name" callback={ this.responseFacebook } />*/ }
             <br/>
             <br/>
             <GoogleLogin clientId="198825134082-5l64c1opmt10ts62nm7ka7dssev9iok9.apps.googleusercontent.com" buttonText="Login with Google" onSuccess={ this.responseGoogle } onFailure={ this.responseGoogle }
